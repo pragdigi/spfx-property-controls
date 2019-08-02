ae7effef9adbb68299ba4d00af237daeb808aef2
Krste <krste@kryptomine.com.au>
2019-08-02 18:01:59 +0200

    impliment control

--- src/webparts/ppControlsTest/PpControlsTestWebPart.ts
+++ src/webparts/ppControlsTest/PpControlsTestWebPart.ts
@@ -9,8 +9,21 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import styles from './PpControlsTestWebPart.module.scss';
 import * as strings from 'PpControlsTestWebPartStrings';
 
+import {
+  PropertyFieldCollectionData,
+  CustomCollectionFieldType
+} from '@pnp/spfx-property-controls/lib/PropertyFieldCollectionData';
+
+import {
+IPropertyFieldGroupOrPerson,
+PropertyFieldPeoplePicker,
+PrincipalType
+} from '@pnp/spfx-property-controls/lib/PropertyFieldPeoplePicker';
+
 export interface IPpControlsTestWebPartProps {
   description: string;
+  people: IPropertyFieldGroupOrPerson[];
+  expansionOpions: any[];
 }
 
 export default class PpControlsTestWebPart extends BaseClientSideWebPart<IPpControlsTestWebPartProps> {
@@ -24,6 +37,8 @@ export default class PpControlsTestWebPart extends BaseClientSideWebPart<IPpCont
               <span class="${ styles.title }">Welcome to SharePoint!</span>
               <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <p class="${ styles.description }">${this.properties.people}</p>
+              <p class="${ styles.description }">${this.properties.expansionOpions}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
                 <span class="${ styles.label }">Learn more</span>
               </a>
@@ -32,6 +47,7 @@ export default class PpControlsTestWebPart extends BaseClientSideWebPart<IPpCont
         </div>
       </div>`;
   }
+
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
@@ -50,7 +66,50 @@ export default class PpControlsTestWebPart extends BaseClientSideWebPart<IPpCont
               groupFields: [
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
-                })
+                }),
+                PropertyFieldPeoplePicker('people', {
+                label: 'PP Field People Picker Control',
+                initialData: this.properties.people,
+                allowDuplicate: false,
+                principalType: [PrincipalType.Users, PrincipalType.SharePoint, PrincipalType.Security],
+                onPropertyChange: this.onPropertyPaneFieldChanged,
+                context: this.context,
+                properties: this.properties,
+                onGetErrorMessage: null,
+                deferredValidationTime: 0,
+                key: 'peopleFieldId'
+                }),
+                PropertyFieldCollectionData('expansionOptions', {
+                  key: 'collectionData',
+                  label: 'Possible expansion options',
+                  panelHeader: 'Possible expansion options',
+                  manageBtnLabel: 'Manage expansion options',
+                  value: this.properties.expansionOpions,
+                  fields: [
+                    {
+                      id: 'Region',
+                      title: 'Region',
+                      required: true,
+                      type: CustomCollectionFieldType.dropdown,
+                      options: [
+                        
+                          { key: 'WA', text: 'Western Australia' },
+                          { key: 'VIC', text: 'Victoria' },
+                          { key: 'NSW', text: 'New South Wales' },
+                          { key: 'QLD', text:'Queensland' },
+                          { key: 'SA', text: 'South Australia' },
+                          { key: 'TAS', text: 'Tasmania' },
+                          { key: 'NT', text: 'Northern Territory' },
+                          { key: 'ACT', text: 'Australian Capital Territory' }
+                      ]
+                    },
+                    {
+                      id: 'Comment',
+                      title: 'Comment',
+                      type: CustomCollectionFieldType.string
+                    }
+                  ]
+                })                        
               ]
             }
           ]
