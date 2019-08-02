f66cc4a355cd8376698edf937c44521c49c478a4
Krste <krste@kryptomine.com.au>
2019-08-02 15:45:32 +0200

    initial commit

---
+++ src/webparts/ppControlsTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPpControlsTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PpControlsTestWebPartStrings' {
+  const strings: IPpControlsTestWebPartStrings;
+  export = strings;
+}
